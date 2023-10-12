7aea00b9ddc02e49f2dac4ff36fccd2706cb2b28
xendalm <adelman2112@gmail.com>
2023-10-12 11:18:48 +0300

    login NotFound error

--- internal/app/authservice/login.go
+++ internal/app/authservice/login.go
@@ -2,8 +2,10 @@ package authservice
 
 import (
 	"context"
+	"errors"
 	authapi "iot-device-tracker-service/pkg/api/auth_service"
 
+	"github.com/jackc/pgx/v4"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -11,6 +13,10 @@ import (
 func (i *Implementation) Login(ctx context.Context, req *authapi.LoginRequest) (*authapi.LoginResponse, error) {
 	user, err := i.dao.GetUser(ctx, req.GetUsername())
 	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, status.Errorf(codes.NotFound, "user does not exist")
+		}
+
 		return nil, status.Errorf(codes.Internal, "cannot find user: %v", err)
 	}
 
